916e5664ef5c3c6a2a789ca2bdc56baf31e3a31f
Robin Kennedy-Reid <rlkennedyreid@protonmail.com>
2020-07-04 19:41:51 +0100

    Removing unecessary bits of docker install script, moving usermod to the end of the script, as changes did not seem to hold.

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 curl -sSL https://get.docker.com | sh
 
-sudo usermod -aG docker pi
-
-docker run hello-world
-
 sudo apt-get install -y libffi-dev libssl-dev
 
 sudo apt-get install -y python3 python3-pip
@@ -12,3 +8,5 @@ sudo apt-get install -y python3 python3-pip
 sudo apt-get remove python-configparser
 
 sudo pip3 install docker-compose
+
+sudo usermod -aG docker pi
